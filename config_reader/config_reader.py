4b8370ee0bc4e0d3318d90c926066c9a3d8a6c05
Leon Eiböck <eiboeck.leon@gmail.com>
2025-11-11 10:34:37 +0100

    PyDoc für config_reader geschrieben

--- config_reader/config_reader.py
+++ config_reader/config_reader.py
@@ -2,12 +2,21 @@ from connector import connector
 import json
 
 class ConfigReader:
-    def __init__(self, dest_path = "../raw_output.txt") -> None:
+    """
+    Reads the configuration file and connects to specified devices in settings.json.
+    It gets their configs and writes them to specified location.
+    """
+    def __init__(self, dest_path: str = "../raw_output.txt") -> None:
         self._commands = None
         self._devices = None
         self._dest_path = dest_path
 
-    def read_settings(self, src ="../settings/settings.json"):
+    def read_settings(self, src: str = "../settings/settings.json") -> None:
+        """
+        Reads settings from given json file
+        :param src: Path of settings json file
+        :return:
+        """
         with open(src, "r", encoding="utf-8") as f:
             data = json.load(f)
             self._devices = data["devices"]
@@ -16,13 +25,18 @@ class ConfigReader:
 
 
     def connect_to_devices(self):
+        """
+        Connects to devices specified in settings.json file.
+        Writes the output to the destination path specified in creating of the object.
+        :return:
+        """
         for ip in self._devices:
             for port in self._devices[ip]:
                 prop = self._devices[ip][port]
                 connection = connector.Connector(prop["device_ios"], ip, port, prop["username"], prop["password"])
                 connection.connect()
                 prompt = connection.conn.find_prompt()
-                #TODO go into privileged mode
+
                 for section in self._commands[prop["device_type"]]:
                     section_responds = ""
                     for command in self._commands[prop["device_type"]][section]:
@@ -33,6 +47,15 @@ class ConfigReader:
                     self.write_to_dest(section_responds, section)
 
     def write_to_dest(self, config: str, section: str) -> bool:
+        """
+        Appends given string to specified destination path and surrounds the string with the section string, z.B.:\n
+        \*** run \***\n
+        hostname R1\n
+        \*** run \***\n
+        :param config: string to be appended to destination path
+        :param section: string to mark beginning and end of section
+        :return:
+        """
         with open(self._dest_path, "a") as dest:
             dest.write(f"** start {section} **\n")
             dest.write(config)
